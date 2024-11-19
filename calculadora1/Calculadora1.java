73c014740b019410d49f89b30c65a811c703b825
LucasTuratti <163550695+LucasTuratti@users.noreply.github.com>
2024-11-18 23:20:24 -0300

    Add files via upload

---
+++ calculadora1/Calculadora1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadora1;
+
+/**
+ *
+ * @author lucas
+ */
+public class Calculadora1 {
+
+    public static void main(String[] args) {
+        Principal2 p = new Principal2();
+        p.setVisible(true);
+    }
+}
